f91d0b2d333a56440d18e6fcfc32d832660a3977
Josue Pachay <pjosuegabriel0204@gmail.com>
2026-02-07 17:12:47 -0500

    Add files via upload

---
+++ Algoritmos.py
@@ -0,0 +1,15 @@
+#Ejercicio 2
+"""print ("Hola Ingresa tu operación Matematica:")
+primval = float(input("Ingrese su primer valor: ") )
+segval = float(input("Ingrese su segundo valor: ") )
+suma = primval + segval
+print("El resultado de tu operacion es: ", suma )"""
+
+#Ejercicio 3
+
+print("Hola Estudiante! Porfavor Ingrese sus notas para calcular su Nota Final")
+aut1 = float(input("Nota de Autonomo 1: "))
+aut2 = float(input("Nota de Autonomo 2: "))
+aut3 = float(input("Nota de Autonomo 3: "))
+notf = (aut1 + aut2 + aut3) / 3
+print(" Su Nota Final por Trabajos Autonomos es ---> ", notf )
