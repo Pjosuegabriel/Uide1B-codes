f17a39d707cad2b5142e12e63a7a22a0891d48a8
Josue Pachay <pjosuegabriel0204@gmail.com>
2026-02-08 00:48:35 -0500

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,4 @@
+print("Bienvenidos a mi Primer Programa")
+print("Mi nombre es: Josue Pachay")
+print("Soy estudiante de la Carrera Sistemas de la Informacion")
+print("Universidad Uide")
---
+++ Variables.py
@@ -0,0 +1,19 @@
+#Nombres
+"""name = input("Ingrese su Nombre: ")
+surname = input("Ingrese su Apellido: ")
+resultado = f" {name}  {surname}"
+print (resultado)"""
+
+#Promedio
+"""nota1 = float(input("Ingrese su primera Nota: "))
+nota2 = float(input("Ingrese su segunda Nota: "))
+nota3 = float(input("Ingrese su tercera Nota: "))
+promedio = (nota1 + nota2 + nota3) / 3
+print(promedio)"""
+
+#Ejercicio
+nomb = input("Ingrese su Primer Nombre: ")
+apell = input("Ingrese su Primer Apellido: ")
+result = f"{nomb} {apell}"
+print("Hola " + result + ", Bienvenido a clase de logica de programación")
+
